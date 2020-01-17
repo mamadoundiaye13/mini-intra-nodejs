55b361e93e5afcd32470be67a10d41523d6c90c9
Mamadou NDIAYE <mamadoundiaye094@gmail.com>
2020-01-17 11:49:36 +0100

    get data module admin from api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {UserService} from './common/user/user.service';
-import {Observable} from 'rxjs';
-import {UserEntity} from './common/user/user.entity';
-import {ApiService} from './common/api/api.service';
-import {error} from 'util';
 
 @Component({
   selector: 'app-root',
@@ -11,21 +6,8 @@ import {error} from 'util';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
-  isAuth = false;
-
   constructor() {
-    setTimeout(
-      () => {
-        this.isAuth = true;
-      }, 4000
-    );
-  }
-
-  onAllumer() {
-    console.log('on allume tout');
   }
   ngOnInit() {
   }
-
 }
